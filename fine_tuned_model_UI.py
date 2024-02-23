910972eea5ec6b86fd4544ff18a9df53f4138407
Srijito354 <133539017+Srijito354@users.noreply.github.com>
2024-02-23 17:59:20 +0530

    Update fine_tuned_model_UI.py

--- fine_tuned_model_UI.py
+++ fine_tuned_model_UI.py
@@ -1,12 +1,18 @@
 import streamlit as st
+import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+#declaring the device
+dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load pre-trained model and tokenizer
 tuned_model = GPT2LMHeadModel.from_pretrained('D:/VSCode files/Nvidia dev contest/mangrove_finetuned_DialoGPT')
 tokenizer = GPT2Tokenizer.from_pretrained('microsoft/DialoGPT-medium')
 
+tuned_model.to(dev)
+
 # Create a text input box for the user to enter their text
-input_text = st.text_input("Enter your text:")
+input_text = st.text_input("Enter your question with respect to freshwater conservation using mangroves: ")
 
 if input_text:
     # Tokenize input text
@@ -14,7 +20,7 @@ if input_text:
     input_ids = input_ids.to(next(tuned_model.parameters()).device)
 
     # Generate response using the model
-    output = tuned_model.generate(input_ids, max_length = 5000)
+    output = tuned_model.generate(input_ids, max_length = 1000)
     output_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
     # Display the model's response
